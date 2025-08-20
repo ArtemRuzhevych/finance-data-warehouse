a0703135b633a1a5bbe37d8b7d1a8bd9f39e9137
Jendd Tema <ruzhevycha@gmail.com>
2025-08-20 19:54:31 +0200

    Continued working on stock loading

--- etl/load_stocks.py
+++ etl/load_stocks.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import sqlalchemy as db
+import sqlalchemy as sql
 import os
 from dotenv import load_dotenv
 
@@ -11,22 +11,24 @@ DATABASE_NAME = os.getenv('DATABASE_NAME')
 DATABASE_USER = os.getenv('DATABASE_USER')
 DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 
-try:
-    db = db.create_engine(f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_SERVER}:{DATABASE_PORT}/{DATABASE_NAME}")
-    print("Database connection established successfully.")
-except Exception as e:
-    print("Error connecting to the database:", e)
-
 def load_stocks(file_path):
     data = pd.read_csv(file_path)
     return data
 
 def insert_data(data):
     try:
+        db = sql.create_engine(f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_SERVER}:{DATABASE_PORT}/{DATABASE_NAME}")
+        print("Database connection established successfully.")
+    except Exception as e:
+        print("Error connecting to the database:", e)
+
+    try:
         data.to_sql('stocks', con=db, if_exists='append', index=False)
         print("Data inserted successfully.")
     except Exception as e:
         print("Error inserting data:", e)
+        db.dispose()
+    db.dispose()
 
 if __name__ == "__main__":
     insert_data(load_stocks('nasdaq100_components.csv'))
